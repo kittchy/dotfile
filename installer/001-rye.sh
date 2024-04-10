68739c406fcc81954a401aeaf1eccfda9ac8cf24
kittchy <kinouchi.takahiro.tg@tut.jp>
2024-04-10 22:00:58 +0900

    [ADD] rye + poetry

--- installer/001-rye.sh
+++ installer/001-rye.sh
@@ -5,6 +5,8 @@ command_name=rye
 if [ $(command_exist $command_name) == "false" ]; then
 	curl -sSf https://rye-up.com/get | bash
 	source "$HOME/.rye/env"
+	# install poetry in rye
+	rye install poetry black flake8 isort
 else
 	echo $command_name" has already been installed"
 fi
