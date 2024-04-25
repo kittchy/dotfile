318ae213f6c7764d333d79e580cc4324d16da463
kittchy <drums.radwimps203@gmail.com>
2024-04-25 22:22:34 +0900

    [FIX] rye installer

--- installer/001-rye.sh
+++ installer/001-rye.sh
@@ -3,7 +3,7 @@
 command_name=rye
 
 if [ $(command_exist $command_name) == "false" ]; then
-	curl -sSf https://rye-up.com/get | RYE_INSTALL_OPTION="--yes" bash
+	curl -sSf https://rye-up.com/get | RYE_NO_AUTO_INSTALL=1 RYE_INSTALL_OPTION="--yes" zsh
 	source "$HOME/.rye/env"
 	# install poetry in rye
 	rye install poetry black ruff isort
