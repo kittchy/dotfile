0b436873509dc92d998a041f7825cffcf1905282
kittchy <drums.radwimps203@gmail.com>
2024-04-25 22:10:45 +0900

    [FIX] ray installer

--- installer/001-rye.sh
+++ installer/001-rye.sh
@@ -3,10 +3,10 @@
 command_name=rye
 
 if [ $(command_exist $command_name) == "false" ]; then
-	curl -sSf https://rye-up.com/get | bash
+	curl -sSf https://rye-up.com/get | RYE_INSTALL_OPTION="--yes" bash
 	source "$HOME/.rye/env"
 	# install poetry in rye
-	rye install poetry black flake8 isort
+	rye install poetry black ruff isort
 else
 	echo $command_name" has already been installed"
 fi
