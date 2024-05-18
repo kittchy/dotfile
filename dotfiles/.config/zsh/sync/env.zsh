400c61bce921b2857d4ed89cb44116cdf6024eab
kittchy <drums.radwimps203@gmail.com>
2024-05-19 00:59:12 +0900

    [FIX] コマンドがなかったときの読み込みエラー修正

--- dotfiles/.config/zsh/sync/env.zsh
+++ dotfiles/.config/zsh/sync/env.zsh
@@ -1,5 +1,5 @@
 zsh $HOME/.config/zsh/sync/detect_os.zsh
-
+zsh $HOME/.config/zsh/sync/command_exists.zsh
 # PATH settings
 PATH=$PATH:~/SCTK/bin
 PATH=$PATH:~/Library/Application\ Support/pypoetry/venv/bin
@@ -13,6 +13,13 @@ export LG_CONFIG_FILE="$HOME/.config/lazygit/config.yaml"
 if [[ $OS == 'mac' ]]; then
   eval $(/opt/homebrew/bin/brew shellenv)
   eval "$(brew shellenv)"
+  source "$(brew --prefix)/share/google-cloud-sdk/path.zsh.inc"
+  source "$(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc"
 fi
+
+if type rbenv > /dev/null 2>&1; then
+  eval "$(rbenv init - zsh)"
+fi
+
 export PATH=$PATH:$HOME/go/bin
 export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig
